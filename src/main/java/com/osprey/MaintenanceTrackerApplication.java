c211478b54fd89ae2e0d393ef44cf8b7eab3d384
Kevin Hudgens <hudgens1073@gmail.com>
2022-06-28 00:07:53 +0900

    main method

---
+++ src/main/java/com/osprey/MaintenanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.osprey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceTrackerApplication.class, args);
+	}
+
+}
