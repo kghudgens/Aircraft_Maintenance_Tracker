b7a6ac3024f0a43fc07a4547766bd65c819bad45
Kevin Hudgens <hudgens1073@gmail.com>
2022-06-28 00:08:18 +0900

    created repository for aircraft

--- src/main/java/com/osprey/repository/AircraftRepository.java
+++ src/main/java/com/osprey/repository/AircraftRepository.java
@@ -3,5 +3,7 @@ package com.osprey.repository;
 import com.osprey.entity.Aircraft;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AircraftRepository extends JpaRepository<Aircraft, Long> {
 }
