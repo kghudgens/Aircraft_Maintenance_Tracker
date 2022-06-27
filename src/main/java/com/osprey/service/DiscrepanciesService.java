93d133a35427902b9002373e5b3add76c106564c
Kevin Hudgens <hudgens1073@gmail.com>
2022-06-28 00:09:45 +0900

    created repository for discrepancies

---
+++ src/main/java/com/osprey/service/DiscrepanciesService.java
@@ -0,0 +1,24 @@
+package com.osprey.service;
+
+import com.osprey.entity.Discrepancies;
+import com.osprey.repository.DiscrepancyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DiscrepanciesService {
+
+    @Autowired
+    private DiscrepancyRepository discrepancyRepository;
+
+
+    public DiscrepanciesService(DiscrepancyRepository discrepancyRepository){
+        this.discrepancyRepository = discrepancyRepository;
+    }
+
+    public List<Discrepancies> findAll() {
+        return discrepancyRepository.findAll();
+    }
+}
