e7157c8dbc0be73f42f10386db4bea0e42cccdc4
Kevin Hudgens <hudgens1073@gmail.com>
2022-07-02 06:19:22 +0900

    created find all method for ac

--- src/main/java/com/osprey/aircraft/AircraftController.java
+++ src/main/java/com/osprey/aircraft/AircraftController.java
@@ -1,8 +1,11 @@
 package com.osprey.aircraft;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("aircraft")
@@ -16,5 +19,10 @@ public class AircraftController {
     public AircraftController(AircraftServices aircraftServices, Aircraft aircraft){
         this.aircraftServices = aircraftServices;
         this.aircraft = aircraft;
+    }
+
+    @GetMapping("/")
+    public List<Aircraft> getAllAircraft(){
+        return aircraftServices.findAll();
     }
 }
