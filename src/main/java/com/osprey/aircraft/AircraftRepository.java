afb5fc634a1add7306122ed8d996227091c13492
Kevin Hudgens <hudgens1073@gmail.com>
2022-07-02 06:39:39 +0900

    created method to find by buno id

--- src/main/java/com/osprey/aircraft/AircraftRepository.java
+++ src/main/java/com/osprey/aircraft/AircraftRepository.java
@@ -2,6 +2,9 @@ package com.osprey.aircraft;
 
 import com.osprey.aircraft.Aircraft;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AircraftRepository extends JpaRepository<Aircraft, Long> {
+
+    Aircraft findAircraftByBuno(int buno);
 }
--- src/main/java/com/osprey/aircraft/AircraftServices.java
+++ src/main/java/com/osprey/aircraft/AircraftServices.java
@@ -21,4 +21,8 @@ public class AircraftServices{
         return aircraftRepository.findAll();
     }
 
+    public Aircraft findAircraftByBuno(int buno) {
+        return aircraftRepository.findAircraftByBuno(buno);
+    }
+
 }
