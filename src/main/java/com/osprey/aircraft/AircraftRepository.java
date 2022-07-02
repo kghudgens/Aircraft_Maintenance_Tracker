2b5aee69b5ec819be1bc703637f77102a8c47949
Kevin Hudgens <hudgens1073@gmail.com>
2022-07-02 20:11:22 +0900

    added correct annotations

--- src/main/java/com/osprey/aircraft/AircraftRepository.java
+++ src/main/java/com/osprey/aircraft/AircraftRepository.java
@@ -3,7 +3,9 @@ package com.osprey.aircraft;
 import com.osprey.aircraft.Aircraft;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AircraftRepository extends JpaRepository<Aircraft, Long> {
 
     Aircraft findAircraftByBuno(int buno);
--- src/main/java/com/osprey/discrepancy/DiscrepancyRepository.java
+++ src/main/java/com/osprey/discrepancy/DiscrepancyRepository.java
@@ -2,6 +2,8 @@ package com.osprey.discrepancy;
 
 import com.osprey.discrepancy.Discrepancies;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DiscrepancyRepository extends JpaRepository<Discrepancies, Long> {
 }
