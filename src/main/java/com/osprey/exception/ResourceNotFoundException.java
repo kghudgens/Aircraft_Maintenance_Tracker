749de94d535c42aac95b00a72ac19e2e209b4e14
Kevin Hudgens <hudgens1073@gmail.com>
2022-07-03 08:53:36 +0900

    made exception public

--- src/main/java/com/osprey/exception/ResourceNotFoundException.java
+++ src/main/java/com/osprey/exception/ResourceNotFoundException.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value= HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
 
-    ResourceNotFoundException(String message) {
+    public ResourceNotFoundException(String message) {
         super(message);
     }
 
