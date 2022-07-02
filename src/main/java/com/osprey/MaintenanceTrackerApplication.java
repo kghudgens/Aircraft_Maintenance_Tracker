a464db881942da4080411c4ad358c62b9892e22d
Kevin Hudgens <hudgens1073@gmail.com>
2022-07-03 08:35:05 +0900

    added configurations to help scan packages

--- src/main/java/com/osprey/MaintenanceTrackerApplication.java
+++ src/main/java/com/osprey/MaintenanceTrackerApplication.java
@@ -1,9 +1,15 @@
 package com.osprey;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories({"com.osprey.aircraft","com.osprey.discrepancy"})
+@ComponentScan( {"com.osprey.aircraft", "com.osprey.exceptions", "com.osprey.discrepancy", "com.osprey.configuration"})
 @SpringBootApplication
+@EnableAutoConfiguration
 public class MaintenanceTrackerApplication {
 
 	public static void main(String[] args) {
