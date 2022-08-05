66ee95c2bb8ba3d3c52cba1c19e9b4f206471e43
Kevin Hudgens <hudgens1073@gmail.com>
2022-08-05 17:47:46 +0900

    ensure the entities are found by sb

--- src/main/java/com/osprey/MaintenanceTrackerApplication.java
+++ src/main/java/com/osprey/MaintenanceTrackerApplication.java
@@ -3,11 +3,13 @@ package com.osprey;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories({"com.osprey.aircraft","com.osprey.discrepancy"})
 @ComponentScan( {"com.osprey.aircraft", "com.osprey.exceptions", "com.osprey.discrepancy", "com.osprey.configuration"})
+@EntityScan({"com.osprey.aircraft", "com.osprey.discrepancy"})
 @SpringBootApplication
 @EnableAutoConfiguration
 public class MaintenanceTrackerApplication {
